59288f0e976d39e4413eb34072bce1f3cae9f930
hsergix <harenasergioasondrazaniaina@gmail.com>
2026-02-08 10:07:48 +0400

    'init'

--- pushFile.py
+++ pushFile.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import subprocess
+import os
 
+
+subprocess.Popen(
+    ["gnome-terminal"],
+    cwd=os.path.expanduser("~/Desktop")
+)
 subprocess.run(["git", "add", "."])
 subprocess.run(["git", "commit", "-m", "'init'"])
 subprocess.run(["git", "push"])
